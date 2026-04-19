6701bc9c0572592e101ed4ffa068590cad6178ef
Thomas Fowler <tfowl95@gmail.com>
2026-04-19 11:37:22 -0500

    added support for date argument in add command

--- cli.py
+++ cli.py
@@ -1,12 +1,20 @@
 import argparse
+from datetime import datetime
+
+def parse_date(input):
+    try:
+        return datetime.strptime(input, "%m/%d/%Y").date()
+    except ValueError:
+        raise argparse.ArgumentTypeError("Use MM/DD/YYYY format")
 
 def cli_arg_parse():
     parser = argparse.ArgumentParser()
-    subparsers = parser.add_subparsers(dest="command")
+    subparser = parser.add_subparsers(dest="command")
 
-# add command
-    add_parser = subparsers.add_parser("add")
-    add_parser.add_argument("--description", type=str, required=True)
-    add_parser.add_argument("--amount", type=float, required=True)
+    # add command
+    add_parser = subparser.add_parser("add")
+    add_parser.add_argument("--description", type = str, required = True)
+    add_parser.add_argument("--amount", type = float, required = True)
+    add_parser.add_argument("--date", type = parse_date)
 
     return parser.parse_args()
--- expense-tracker.py
+++ expense-tracker.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python3
 
-import csv
 from cli import cli_arg_parse
 from storage import get_file_path, create_file_if_missing, get_csv_contents
 
 args = cli_arg_parse()
+print(args.date)
 file_path = get_file_path()
 create_file_if_missing(file_path)
 
 expenses = get_csv_contents(file_path)
-
-for row in expenses:
-    print(row)
