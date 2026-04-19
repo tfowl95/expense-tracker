23339d837b8e9f27c826c1165f1d7aac89619d1e
Thomas Fowler <tfowl95@gmail.com>
2026-04-19 11:00:41 -0500

    added ability to read .csv file as input into a list

--- expense-tracker.py
+++ expense-tracker.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 
+import csv
 from cli import cli_arg_parse
-from storage import set_output_path, create_file_if_missing
+from storage import get_file_path, create_file_if_missing, get_csv_contents
 
 args = cli_arg_parse()
-output_file = set_output_path()
-create_file_if_missing(output_file)
+file_path = get_file_path()
+create_file_if_missing(file_path)
+
+expenses = get_csv_contents(file_path)
+
+for row in expenses:
+    print(row)
--- storage.py
+++ storage.py
@@ -1,6 +1,7 @@
 from pathlib import Path
+import csv
 
-def set_output_path():
+def get_file_path():
     script_dir = Path(__file__).resolve().parent
     return script_dir / "expenses.csv"
 
@@ -8,3 +9,7 @@ def create_file_if_missing(output_file):
     if not Path(output_file).exists():
         with open(output_file, "w") as file:
             file.close()
+
+def get_csv_contents(file_path):
+    with open(file_path, "r") as file:
+        return list(csv.reader(file))
